f9800b61dc73bfa404c4c5561d0a32ad4236d420
rmatei <rmatei@pcs-publishing.com>
2020-01-12 20:22:29 +0000

    configuring log level

--- logger.js
+++ logger.js
@@ -1,9 +1,10 @@
 'use strict';
 
+const logLevel = require('config').get('app.logLevel');
 const log4js = require('log4js');
 log4js.configure({
     appenders: { console: { type: 'console' } },
-    categories: { default: { appenders: ['console'], level: 'debug' } }
+    categories: { default: { appenders: ['console'], level: logLevel } }
 });
 
 const loggerInstance = log4js.getLogger();
